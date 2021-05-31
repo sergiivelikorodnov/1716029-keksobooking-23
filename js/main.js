1db9c2c260c1275dec7e4be631c6e8be013a3eef
Sergii Velikorodnov <sergii.velikorodnov@gmail.com>
2021-05-31 14:04:34 +0200

    Changed functions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 //***https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
 
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max + 1);
-  if (min < 0 || max < 0) {
+const getRundomNumber = (min, max, exp = 0) => {
+  if (min < 0 || max < 0 || exp <= 0) {
     return 'Ошибка. Все числа должны быть положительные';
   }
 
@@ -11,28 +9,15 @@ const getRandomInt = (min, max) => {
     return 'Ошибка. Второе число должно быть больше первого';
   }
 
-  return Math.floor(Math.random() * (max - min) + min);
-};
-
-
-const getRandomFloat = (min, max, exp = 2) => {
-  if (typeof min === 'undefined') {
-    return 'Ошибка. Введите число';
-  }
-
-  if (min < 0 || max < 0 || exp < 0) {
-    return 'Ошибка. Все числа должны быть положительные';
-  }
-
-  if (min > max) {
-    return 'Ошибка. Второе число должно быть больше первого';
-  }
-
-  if (exp === 0) {
-    return Math.random() * (max - min) + min;
-  }
-
   return (Math.random() * (max - min) + min).toFixed(exp);
 };
 
-//console.log(getRandomFloat(1, 10, 4));
+const getRandomInt = (min, max) => {
+  max = max + 1;
+  return getRundomNumber(min, max, 0);
+};
+
+const getRandomFloat = (min, max, exp) => getRundomNumber(min, max, exp);
+
+getRandomInt(0, 3);
+getRandomFloat(0, 3, 3);
