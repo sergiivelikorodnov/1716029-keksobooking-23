d82cc01b313973e24697b5ec6b69337a2d26bbd0
Sergii Velikorodnov <sergii.velikorodnov@gmail.com>
2021-06-24 15:59:06 +0200

    Added messages, fixed address

--- js/submit-form.js
+++ js/submit-form.js
@@ -1,4 +1,5 @@
-import { MIN_NAME_LENGTH, MAX_NAME_LENGTH, MAX_ROOM_PRICE, MIN_BUNGALOW_PRICE, MIN_FLAT_PRICE, MIN_HOTEL_PRICE, MIN_HOUSE_PRICE, MIN_PALACE_PRICE, DEFAULT_ROOM_NUMBER, DEFAULT_ROOM_CAPACITY, MAX_ROOM_NUMBER } from './constants.js';
+
+import { MIN_NAME_LENGTH, MAX_NAME_LENGTH, MAX_ROOM_PRICE, MIN_BUNGALOW_PRICE, MIN_FLAT_PRICE, MIN_HOTEL_PRICE, MIN_HOUSE_PRICE, MIN_PALACE_PRICE, DEFAULT_ROOM_NUMBER, DEFAULT_ROOM_CAPACITY, MAX_ROOM_NUMBER, ROOM_VAL_MESSAGE, ROOM_PRICE_VAL_MESSAGE } from './constants.js';
 
 const offerName = document.querySelector('#title');
 const priceRoom = document.querySelector('#price');
@@ -7,14 +8,14 @@ const roomNumber = document.querySelector('#room_number');
 const roomCapacity = document.querySelector('#capacity');
 const roomAddress = document.querySelector('#address');
 
-roomAddress.setAttribute('value', '35.68950, 139.70171'); пше
+roomAddress.value = '35.68950, 139.70171';
 
 let minPriceRoom = MIN_FLAT_PRICE;
 let roomNumberValue = DEFAULT_ROOM_NUMBER;
 let roomCapacityValue = DEFAULT_ROOM_CAPACITY;
 
 if (roomNumberValue < roomCapacityValue) {
-  roomCapacity.setCustomValidity('Слишком много гостей. Выберите меньше значение');
+  roomCapacity.setCustomValidity(ROOM_VAL_MESSAGE[0]);
   roomCapacity.reportValidity();
 }
 
@@ -63,26 +64,31 @@ priceRoom.addEventListener('input', () => {
 
 roomNumber.addEventListener('change', () => {
   roomNumberValue = Number(roomNumber.value);
+  let message;
+
   if (roomNumberValue < roomCapacityValue) {
-    roomCapacity.setCustomValidity('Слишком много гостей. Выберите меньше значение');
+    message = ROOM_VAL_MESSAGE[0];
   } else if (roomNumberValue === MAX_ROOM_NUMBER && roomCapacityValue !== 0) {
-    roomCapacity.setCustomValidity('Выберите опцию "Не для гостей"');
+    message = ROOM_VAL_MESSAGE[1];
   } else if (roomNumberValue < MAX_ROOM_NUMBER && roomCapacityValue === 0) {
-    roomCapacity.setCustomValidity('Выберите количество гостей');
+    message = ROOM_VAL_MESSAGE[2];
   } else {
-    roomCapacity.setCustomValidity('');
+    message = ROOM_VAL_MESSAGE[3];
   }
+  roomCapacity.setCustomValidity(message);
   roomCapacity.reportValidity();
 });
 
 roomCapacity.addEventListener('change', () => {
   roomCapacityValue = Number(roomCapacity.value);
+  let message;
   if (roomNumberValue < roomCapacityValue) {
-    roomCapacity.setCustomValidity('Слишком много гостей. Выберите меньше значение');
+    message = ROOM_VAL_MESSAGE[0];
   } else if (roomNumberValue < MAX_ROOM_NUMBER && roomCapacityValue === 0) {
-    roomCapacity.setCustomValidity('Выберите количество гостей');
+    message = ROOM_VAL_MESSAGE[2];
   } else {
-    roomCapacity.setCustomValidity('');
+    message = ROOM_VAL_MESSAGE[3];
   }
+  roomCapacity.setCustomValidity(message);
   roomCapacity.reportValidity();
 });
