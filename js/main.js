d1fa77fab1c2c91fe2fbea29f944494540b89661
Sergii Velikorodnov <sergii.velikorodnov@gmail.com>
2021-07-14 14:56:39 +0200

    Fixes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import './map.js';
 import './offer.js';
 import './submit-form.js';
-import './filter.js';
 import { outputProperties } from './filter.js';
 import { offerFormSubmit, resetForm } from './submit-form.js';
 
